6362e9a5bc026ca55fe2a1d46fe774de8923ae1e
egsouzaster <egsouzaster@gmail.com>
2020-08-31 00:16:32 -0300

    Template do Site

---
+++ views-cache/header.ed528805425869f03885c566fea4f241.rtpl.php
@@ -0,0 +1,11 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ExpertTIcommerce</title>
+	</head>
+		<body>
+			<h1>Cursos na área de tecnologia</h1>
+		</body>
+</html>
+		
